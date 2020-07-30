01b02ea439d525b77e93a9f431cf7e0b53cd444c
Glumbatsch <davidsiepen95@gmail.com>
2020-07-31 00:34:56 +0200

    clean up

---
+++ CudaRayTracer/src/Util.h
@@ -0,0 +1,130 @@
+#pragma once
+#define WIN32_LEAN_AND_MEAN
+#include <Windows.h>
+#include <stdio.h>
+#include "OpenImageDenoise\oidn.h"
+#include "stb_image_writer.h"
+#include "Structs.h"
+#include <chrono>
+
+#ifndef _DEBUG
+// Release
+#define cudaCall(x) x
+#else
+// Debug
+#define cudaCall(x) (assert(x == cudaSuccess))
+#endif 
+
+#define GetIndex() (threadIdx.x + blockIdx.x * blockDim.x)
+#define IsOutOfBounds(id, image) (id >= image->width * image->height)
+
+void LoadConfig(const char* fileName, Config& cfg)
+{
+	cfg.imageWidth = GetPrivateProfileInt("Image", "imageWidth",
+		1024, fileName);
+	cfg.imageHeight = GetPrivateProfileInt("Image", "imageHeight",
+		720, fileName);
+
+	cfg.samplesPerPixel = GetPrivateProfileInt("RT",
+		"samplesPerPixel", 32, fileName);
+	cfg.maxBounces = GetPrivateProfileInt("RT", "maxBounces",
+		16, fileName);
+	cfg.bDenoise = GetPrivateProfileInt("RT", "bDenoise",
+		1, fileName);
+
+
+	printf("Starting the Path tracer with the following configuration:\n\n");
+	printf("\tImage: %dx%d\n", cfg.imageWidth, cfg.imageHeight);
+	printf("\tSamples per pixel: %d\n", cfg.samplesPerPixel);
+	printf("\tMaximum bounces per path: %d\n", cfg.maxBounces);
+	const char en[] = "enabled";
+	const char dis[] = "disabled";
+	const char* denoiseString = cfg.bDenoise ? en : dis;
+	printf("\tDenoising is %s\n", denoiseString);
+	printf("\n");
+}
+// float3 to packed ABGR
+int PackColor(float3 color)
+{
+	int r = (int)(255.9f * color.x);
+	int g = (int)(255.9f * color.y);
+	int b = (int)(255.9f * color.z);
+
+	int result = 0xFF000000; // alpha
+	result += (unsigned char)b << 16;
+	result += (unsigned char)g << 8;
+	result += (unsigned char)r;
+	return result;
+}
+
+void WriteBMP(Config cfg, float3* deviceImage)
+{
+	printf("Writing image to file...");
+	auto startTime = std::chrono::high_resolution_clock::now();
+
+	int rayCount = cfg.imageWidth * cfg.imageHeight;
+	size_t floatImageSize = sizeof(float3) * rayCount;
+	
+	// OIDN does not like pinned memory
+	float3* fpixels = (float3*)malloc(floatImageSize);
+	//cudaCall(cudaMallocHost(&fpixels,floatImageSize));
+	int* pixels = (int*)malloc(sizeof(int) * rayCount);
+	cudaCall(cudaMemcpy(fpixels, deviceImage,
+		floatImageSize, cudaMemcpyDeviceToHost));
+
+	int imageWidth = cfg.imageWidth;
+	int imageHeight = cfg.imageHeight;
+
+	// #Denoiser
+	if (cfg.bDenoise)
+	{
+		OIDNDevice device = oidnNewDevice(OIDN_DEVICE_TYPE_DEFAULT);
+		oidnCommitDevice(device);
+
+		OIDNFilter filter = oidnNewFilter(device, "RT");
+		oidnSetSharedFilterImage(filter, "color", fpixels,
+			OIDN_FORMAT_FLOAT3, imageWidth, imageHeight, 0, 0, 0);
+		oidnSetSharedFilterImage(filter, "output", fpixels,
+			OIDN_FORMAT_FLOAT3, imageWidth, imageHeight, 0, 0, 0);
+		oidnSetFilter1b(filter, "hdr", false);
+		oidnCommitFilter(filter);
+
+		oidnExecuteFilter(filter);
+
+		const char* errorMessage;
+		if (oidnGetDeviceError(device, &errorMessage) !=
+			OIDN_ERROR_NONE)
+		{
+			printf("Error: %s\n", errorMessage);
+		}
+		oidnReleaseFilter(filter);
+		oidnReleaseDevice(device);
+	}
+
+	for (int y = 0; y < imageHeight; y++)
+	{
+		for (int x = 0; x < imageWidth; x++)
+		{
+			int id = x + imageWidth * y;
+			pixels[id] = PackColor(fpixels[id]);
+		}
+	}
+
+	// 4 -> ABGR
+	stbi_flip_vertically_on_write(true);
+	stbi_write_bmp("image.bmp", imageWidth, imageHeight, 4,
+		(void*)pixels);
+
+	auto endTime = std::chrono::high_resolution_clock::now();
+	auto mili = std::chrono::duration_cast<std::chrono::milliseconds>(endTime - startTime).count();
+	printf(" done! That took %3.3f seconds.\n", (double)mili / 1000.0);
+}
+
+Plane CreatePlane(const float3& a, const float3& b, const float3& c)
+{
+	Plane p;
+	p.normal = Normalized(Cross(b - a, c - a));
+	p.d = Dot(p.normal, a);
+	p.materialIndex = 0;
+	return p;
+}
--- CudaRayTracer/src/kernel.cu
+++ CudaRayTracer/src/kernel.cu
@@ -1,40 +1,20 @@
-#define WIN32_LEAN_AND_MEAN
-#include <Windows.h>
-
 #include "cuda_runtime.h"
 #include "device_launch_parameters.h"
 #include "Structs.h"
 #include "IntersectionTests.h"
-#include "stb_image_writer.h"
+#include "Util.h"
 
 #include <stdio.h>
 #include <assert.h>
 #include <cstdlib>
 #include <float.h>
-#include <chrono>
 
 #include <curand.h>
 #include <curand_kernel.h>
 
 #include "OpenImageDenoise\oidn.h"
 
-#define DENOISE_FINAL_IMAGE
-
-#define ARRAY_LENGTH(arr) (sizeof(arr)/sizeof((arr)[0]))
-
-#ifndef _DEBUG
-// Release
-#define cudaCall(x) x
-#else
-// Debug
-#define cudaCall(x) (assert(x == cudaSuccess))
-#endif 
-
-#define GetIndex() (threadIdx.x + blockIdx.x * blockDim.x)
-#define IsOutOfBounds(id, image) (id >= image->width * image->height)
-
 Config g_cfg;
-int g_rayCount;
 float3* g_deviceImage;
 // Device Stuff
 World* d_world;
@@ -57,117 +37,6 @@ __global__ void ShadeIntersectionsKernel(DeviceImage* image,
 __global__ void WriteRayColorToImage(DeviceImage* image,
 	World* world, float contributionPerPixel);
 
-void LoadConfig(const char* fileName, Config& cfg)
-{
-	cfg.imageWidth = GetPrivateProfileInt("Image", "imageWidth", 
-		1024, fileName);
-	cfg.imageHeight = GetPrivateProfileInt("Image", "imageHeight",
-		720, fileName);
-
-	cfg.samplesPerPixel = GetPrivateProfileInt("RT", 
-		"samplesPerPixel", 32, fileName);
-	cfg.maxBounces = GetPrivateProfileInt("RT", "maxBounces",
-		16, fileName);
-	cfg.bDenoise = GetPrivateProfileInt("RT", "bDenoise",
-		1, fileName);
-
-
-	printf("Starting the Path tracer with the following configuration:\n\n");
-	printf("\tImage: %dx%d\n", cfg.imageWidth, cfg.imageHeight);
-	printf("\tSamples per pixel: %d\n", cfg.samplesPerPixel);
-	printf("\tMaximum bounces per path: %d\n",cfg.maxBounces);
-	const char en[] = "enabled";
-	const char dis[] = "disabled";
-	const char* denoiseString = cfg.bDenoise ? en : dis;
-	printf("\tDenoising is %s\n", denoiseString);
-	printf("\n");
-}
-
-// float3 to packed ABGR
-int PackColor(float3 color)
-{
-	int r = (int)(255.9f * color.x);
-	int g = (int)(255.9f * color.y);
-	int b = (int)(255.9f * color.z);
-
-	int result = 0xFF000000; // alpha
-	result += (unsigned char)b << 16;
-	result += (unsigned char)g << 8;
-	result += (unsigned char)r;
-	return result;
-}
-
-void WriteBMP()
-{
-	printf("Writing image to file...");
-	auto startTime = std::chrono::high_resolution_clock::now();
-
-	size_t floatImageSize = sizeof(float3) * g_rayCount;
-	// OIDN seems not to like pinned memory - difference is just 
-	// ~1.5s on 1920x1080 dbg
-	float3* fpixels = (float3*)malloc(floatImageSize);
-	//cudaCall(cudaMallocHost(&fpixels,floatImageSize));
-	int* pixels = (int*)malloc(sizeof(int) * g_rayCount);
-	cudaCall(cudaMemcpy(fpixels, g_deviceImage,
-		floatImageSize, cudaMemcpyDeviceToHost));
-
-	int imageWidth = g_cfg.imageWidth;
-	int imageHeight = g_cfg.imageHeight;
-
-	// #Denoiser
-	if (g_cfg.bDenoise)
-	{
-		OIDNDevice device = oidnNewDevice(OIDN_DEVICE_TYPE_DEFAULT);
-		oidnCommitDevice(device);
-
-		OIDNFilter filter = oidnNewFilter(device, "RT");
-		oidnSetSharedFilterImage(filter, "color", fpixels,
-			OIDN_FORMAT_FLOAT3, imageWidth, imageHeight, 0, 0, 0);
-		oidnSetSharedFilterImage(filter, "output", fpixels,
-			OIDN_FORMAT_FLOAT3, imageWidth, imageHeight, 0, 0, 0);
-		oidnSetFilter1b(filter, "hdr", false);
-		oidnCommitFilter(filter);
-
-		oidnExecuteFilter(filter);
-
-		const char* errorMessage;
-		if (oidnGetDeviceError(device, &errorMessage) != 
-			OIDN_ERROR_NONE)
-		{
-			printf("Error: %s\n", errorMessage);
-		}
-		oidnReleaseFilter(filter);
-		oidnReleaseDevice(device);
-	}
-
-	for (int y = 0; y < imageHeight; y++)
-	{
-		for (int x = 0; x < imageWidth; x++)
-		{
-			int id = x + imageWidth * y;
-			pixels[id] = PackColor(fpixels[id]);
-		}
-	}
-
-	// 4 -> ABGR
-	stbi_flip_vertically_on_write(true);
-	stbi_write_bmp("image.bmp", imageWidth, imageHeight, 4,
-		(void*)pixels);
-
-	auto endTime = std::chrono::high_resolution_clock::now();
-	auto mili = std::chrono::duration_cast<std::chrono::milliseconds>(endTime - startTime).count();
-	printf(" done! That took %3.3f seconds.\n",(double)mili/1000.0);
-}
-
-Plane CreatePlane(const float3& a, const float3& b, const float3& c)
-{
-	Plane p;
-	p.normal = Normalized(Cross(b - a, c - a));
-	p.d = Dot(p.normal, a);
-	p.materialIndex = 0;
-	return p;
-}
-
 int main()
 {
 	LoadConfig("./rt.ini",g_cfg);
@@ -175,7 +44,7 @@ int main()
 	CudaInit();
 	Raytrace();
 	cudaCall(cudaDeviceSynchronize());
-	WriteBMP();
+	WriteBMP(g_cfg,g_deviceImage);
 	
 	std::system("start image.bmp");
 	return 0;
@@ -183,7 +52,7 @@ int main()
 
 void CudaInit()
 {
-	g_rayCount = g_cfg.imageWidth * g_cfg.imageHeight;
+	int rayCount = g_cfg.imageWidth * g_cfg.imageHeight;
 
 	Material materials[6] = {};
 	materials[0].emitColor = make_float3(0.1f, 0.4f, 0.5f) * 0.0f;
@@ -239,15 +108,15 @@ void CudaInit()
 	w.materialCount = sizeof(materials) / sizeof(materials[0]);
 	w.sphereCount = sizeof(spheres) / sizeof(spheres[0]);
 	w.planeCount = sizeof(planes) / sizeof(planes[0]);
-	w.rayCount = g_rayCount;
+	w.rayCount = rayCount;
 
 	cudaCall(cudaMalloc(&d_world, sizeof(World)));
 	cudaCall(cudaMalloc(&w.materials, sizeof(materials)));
 	cudaCall(cudaMalloc(&w.planes, sizeof(planes)));
 	cudaCall(cudaMalloc(&w.spheres, sizeof(spheres)));
-	cudaCall(cudaMalloc(&w.rays, sizeof(Ray) * g_rayCount));
+	cudaCall(cudaMalloc(&w.rays, sizeof(Ray) * rayCount));
 	cudaCall(cudaMalloc(&w.intersections,
-		sizeof(Intersection) * g_rayCount));
+		sizeof(Intersection) * rayCount));
 
 
 	cudaCall(cudaMemcpy(d_world, &w, sizeof(World),
@@ -288,11 +157,11 @@ void CudaInit()
 	}
 
 	cudaCall(cudaMalloc(&image.pixels,
-		sizeof(float3) * g_rayCount));
+		sizeof(float3) * rayCount));
 	g_deviceImage = image.pixels;
 	// init image to black
 	cudaCall(cudaMemset(g_deviceImage, 0,
-		sizeof(float3) * g_rayCount));
+		sizeof(float3) * rayCount));
 
 
 	cudaCall(cudaMalloc(&d_image, sizeof(DeviceImage)));
@@ -300,7 +169,7 @@ void CudaInit()
 		cudaMemcpyHostToDevice));
 
 	cudaCall(cudaMalloc(&d_randStates,
-		sizeof(curandState) * g_rayCount));
+		sizeof(curandState) * rayCount));
 }
 
 __global__ void InitCurandKernel(int rayCount,
@@ -433,13 +302,14 @@ void Raytrace()
 
 	int imageWidth = g_cfg.imageWidth;
 	int imageHeight = g_cfg.imageHeight;
+	int rayCount = imageWidth * imageHeight;
 
 	int blockCount = imageWidth * imageHeight / threadCount + 1;
 
 	printf("Initializing cuRand states... ");
 	// #Time
 	auto startTime = std::chrono::high_resolution_clock::now();
-	InitCurandKernel << <blockCount, threadCount >> > (g_rayCount, d_randStates, 1234);
+	InitCurandKernel << <blockCount, threadCount >> > (rayCount, d_randStates, 1234);
 	cudaCall(cudaDeviceSynchronize());
 	auto endTime = std::chrono::high_resolution_clock::now();
 	auto mili = std::chrono::duration_cast<std::chrono::milliseconds>(endTime - startTime).count();
@@ -475,4 +345,3 @@ void Raytrace()
 	mili = std::chrono::duration_cast<std::chrono::milliseconds>(endTime - startTime).count();
 	printf(" done! That took %3.3f seconds.\n", (double)mili / 1000.0);
 }
-// #Todo remove unnecessary parameters from kernels (e.g. image)
